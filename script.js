5a3ab524ccef8fa1db12550ad83dea1b31880d91
The Sophile <abhinavpb249@gmail.com>
2023-07-11 04:42:28 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+var hamburger = document.getElementsByClassName('hamburger')[0];
+const option = document.getElementById("option-this");
+
+// hamburger.addEventListener('click', function(){
+    option.style.bottom = "10px";
+// })
